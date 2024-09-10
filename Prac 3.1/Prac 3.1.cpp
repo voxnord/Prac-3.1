7570d7dddec73c7307f06086953fdc172e54b5ba
knox <nikita.3.khorev@gmail.com>
2024-09-10 12:17:38 +0300

    sorts

--- Prac 3.1/Prac 3.1.cpp
+++ Prac 3.1/Prac 3.1.cpp
@@ -11,6 +11,7 @@ class n1
 private:
     int i = 0;
 public:
+    // O(n)
     int n1_1(int arr[], int n)
     {
         int Max = 0;
@@ -22,7 +23,7 @@ public:
 
         return Max;
     }
-
+    // O(n)
     int n1_2(int arr[], int n)
     {
         int sum = 0;
@@ -34,7 +35,7 @@ public:
 
         return sum;
     }
-
+    // O(n)
     bool n1_3(int arr[], int n, int search_num)
     {
         for (i; i < n; i++)
@@ -51,59 +52,61 @@ public:
     }
 };
 
-double bubbleSort(vector<int> arr, int n)
+class sorts
 {
-    for (int i = 0; i < n; i++)
+public:
+    // O(n^2)
+    auto bubbleSort(vector<int> arr, int n)
     {
-        for (int j = 0; j < n - 1; j++)
+        auto start_time = std::chrono::steady_clock::now();
+
+        for (int i = 0; i < n; i++)
         {
-            if (arr[j] > arr[j + 1])
+            for (int j = 0; j < n - 1; j++)
             {
-                int b = arr[j]; // создали дополнительную переменную
-                arr[j] = arr[j + 1]; // меняем местами
-                arr[j + 1] = b; // значения элементов
+                if (arr[j] > arr[j + 1])
+                {
+                    int b = arr[j]; // создали дополнительную переменную
+                    arr[j] = arr[j + 1]; // меняем местами
+                    arr[j + 1] = b; // значения элементов
+                }
             }
         }
+
+        auto end_time = chrono::steady_clock::now();
+        auto elapsed_s = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time);
+
+        return elapsed_s.count();
     }
-
-    auto start_time = std::chrono::steady_clock::now();
-
-    delete new int(1);
-
-    auto end_time = chrono::steady_clock::now();
-    auto elapsed_s = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time);
-
-    return elapsed_s.count();
-}
-
-double choiceSort(vector<int> arr, int n)
-{
-    for (int i = 0; i < n - 1; i++)
+    // O(n^2)
+    auto choiceSort(vector<int> arr, int n)
     {
-        int min_index = i;
-        for (int j = i + 1; j < n; j++)
+        auto start_time = std::chrono::steady_clock::now();
+
+        for (int i = 0; i < n - 1; i++)
         {
-            if (arr[j] < arr[min_index])
+            int min_index = i;
+            for (int j = i + 1; j < n; j++)
             {
-                min_index = j;
+                if (arr[j] < arr[min_index])
+                {
+                    min_index = j;
+                }
+            }
+            if (min_index != i)
+            {
+                swap(arr[i], arr[min_index]);
             }
         }
-        if (min_index != i)
-        {
-            swap(arr[i], arr[min_index]);
-        }
+
+        auto end_time = chrono::steady_clock::now();
+        auto elapsed_s = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time);
+
+        return elapsed_s.count();
     }
+};
 
-    auto start_time = std::chrono::steady_clock::now();
-
-    delete new int(1);
-
-    auto end_time = chrono::steady_clock::now();
-    auto elapsed_s = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time);
-
-    return elapsed_s.count();
-}
-
+//O(n)
 string n3_1_1(string str[], int n)
 {
     string maxStr = "";
@@ -125,6 +128,10 @@ string n3_1_1(string str[], int n)
     return secondMax;
 }
 
+string n3_1_2(string str[], int n)
+{
+
+}
 
 void n3_2()
 {
@@ -157,8 +164,19 @@ int main()
 
     cout << endl;
 
-    cout << "Пузырьковая сортировка заняла " << bubbleSort(arr2, n2) << " наносекунд" << endl;
-    cout << "Сортировка выбором заняла " << choiceSort(arr2, n2) << " наносекунд" << endl;
+    sorts num2;
+
+    cout << "Пузырьковая сортировка заняла " << num2.bubbleSort(arr2, n2) << " наносекунд" << endl;
+    cout << "Сортировка выбором заняла " << num2.choiceSort(arr2, n2) << " наносекунд" << endl;
+
+    if (num2.bubbleSort(arr2, n2) > num2.choiceSort(arr2, n2))
+    {
+        cout << "Сортировка вставками эффективнее, чем пузырьковая сортирвка" << endl;
+    }
+    else if (num2.bubbleSort(arr2, n2) < num2.choiceSort(arr2, n2))
+    {
+        cout << "Пузырьковая сортировка эффективнее, чем сортирвка вставками" << endl;
+    }
 
     string str1[]
     {
